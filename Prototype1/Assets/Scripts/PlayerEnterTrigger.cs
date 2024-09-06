8e3c916e5d68e6ab71c869e82871412dd23f6c46
TIGERJ1N <53501028+TIGERJ1N@users.noreply.github.com>
2024-09-06 10:15:42 -0500

    Worked on Prototype1
    Worked on the project a bit more. Now prepping to add all of the things the "Lead Designer" asked for.

---
+++ Prototype1/Assets/Scripts/PlayerEnterTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Attach this to the player
+public class PlayerEnterTrigger : MonoBehaviour
+{
+    // Set this reference in the Inspector
+    public Text textbox;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TriggerZone"))
+        {
+            textbox.text = "You win!";
+        }
+    }
+
+}
