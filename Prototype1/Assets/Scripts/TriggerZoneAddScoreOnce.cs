22e631bfb9c7a006e0a4903f58b1bec3bf262ba1
TIGERJ1N <53501028+TIGERJ1N@users.noreply.github.com>
2024-09-06 20:37:22 -0500

    Added Prototype 1 Midway Progress

---
+++ Prototype1/Assets/Scripts/TriggerZoneAddScoreOnce.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Attach this script to a Trigger Zone so that each Trigger Zone only triggers once
+public class TriggerZoneAddScoreOnce : MonoBehaviour
+{
+    private bool triggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            ScoreManager.score++;
+        }
+    }
+}
