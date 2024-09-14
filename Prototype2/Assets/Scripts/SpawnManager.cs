29036f43ec8f92c72dd8cf01d771a9887e6bded2
TIGERJ1N <53501028+TIGERJ1N@users.noreply.github.com>
2024-09-14 14:49:02 -0500

    Prototype2 Progress4
    Created the SpawnManager for the animal prefabs. It controls how often an animal will spawn using a randomized value in a small range. Applied the SpawnManager to the animal prefabs.

---
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Set this array of references in the inspector
+    public GameObject[] prefabsToSpawn;
+
+    // Variables for Spawn Position
+    private float leftBound = -14;
+    private float rightBound = 14;
+    private float spawnPosZ = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This if goes un-used because we moved a lot of things out of it in Steps 5 and 6.
+        // if (Input.GetKeyDown(KeyCode.S))
+        {
+            // Step 1:
+            // Spawn 0th prefab at top of screen
+            // Instantiate(prefabsToSpawn[0], new Vector3(0, 0, 20), prefabsToSpawn[0].transform.rotation);
+
+            // Step 2:
+            // Pick a random index between 0 and the length of the array
+            // int prefabIndex = Random.Range(0, prefabsToSpawn.Length);
+
+            // Instantiate(prefabsToSpawn[prefabIndex], new Vector3(0, 0, 20), prefabsToSpawn[prefabIndex].transform.rotation);
+
+            // Step 3:
+            // Randomly generate index and spawn position
+
+            // Generate index -- pick a random animal index
+            // int prefabIndex = Random.Range(0, prefabsToSpawn.Length);
+
+            // Generate spawn position -- pick a random position for the animal
+            // Vector3 spawnPos = new Vector3(Random.Range(leftBound, rightBound), 0, spawnPosZ);
+
+            // Spawn our animals
+            // Instantiate(prefabsToSpawn[prefabIndex], spawnPos, prefabsToSpawn[prefabIndex].transform.rotation);
+
+            // Step 4
+            // SpawnRandomPrefab();
+        }
+    }
+
+    // Step 5: add void Start() method so that the program continually spawns new animals
+    private void Start()
+    {
+        // Step 5: InvokeRepeating() calls the method repeatedly: ("methodName", startDelay, spawnInterval);
+        // InvokeRepeating("SpawnRandomPrefab", 2, 1.5f);
+
+        // Step 6: We get rid of InvokeRepeating, instead using a Coroutine (SpawnRandomPrefabWithCoroutine()) to spawn the prefabs
+        StartCoroutine(SpawnRandomPrefabWithCoroutine());
+
+    }
+
+    // Step 6: Create a new method, return type IEnumerator with yield return new WaitForSeconds(); to wait a few seconds at the start of the program before spawning prefabs
+    IEnumerator SpawnRandomPrefabWithCoroutine()
+    {
+        // Add a three second delay before first spawning the prefabs
+        yield return new WaitForSeconds(3f);
+
+        while (true)
+        {
+            // Instead of copy-pasting, call the encapsulated method that was already written
+            SpawnRandomPrefab();
+
+            // Creates a randomized delay so that animals spawn with random timings
+            float randomDelay = Random.Range(1.5f, 3.0f);
+
+            // Wait for a random amount of seconds (0.8s to 2.0s) before continuing the while loop (wait some seconds to spawn another prefab)
+            yield return new WaitForSeconds(randomDelay);
+        }
+    }
+
+    // Step 4: Move all of the code from Step 3 down to it's own method, SpawnRandomPrefab()
+    void SpawnRandomPrefab()
+    {
+        // The below comes from Step 3 above and is unchanged
+        // Generate index -- pick a random animal index
+        int prefabIndex = Random.Range(0, prefabsToSpawn.Length);
+
+        // Generate spawn position -- pick a random position for the animal
+        Vector3 spawnPos = new Vector3(Random.Range(leftBound, rightBound), 0, spawnPosZ);
+
+        // Spawn our animals
+        Instantiate(prefabsToSpawn[prefabIndex], spawnPos, prefabsToSpawn[prefabIndex].transform.rotation);
+    }
+}
