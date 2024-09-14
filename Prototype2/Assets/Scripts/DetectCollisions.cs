7b12afec4eeebcac16a2c7f00e27dfcedca21887
TIGERJ1N <53501028+TIGERJ1N@users.noreply.github.com>
2024-09-14 15:05:03 -0500

    Prototype2 Progress5
    Added the DisplayScore.cs script so that we have a "Score: 0" text that updates in the top left corner of the UI. Also updated DetectCollisions.cs to handle when the food prefab interacts with an animal, updating the score by incrementing it by 1.

--- Prototype2/Assets/Scripts/DetectCollisions.cs
+++ Prototype2/Assets/Scripts/DetectCollisions.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 // Attach to food projectile prefab
 public class DetectCollisions : MonoBehaviour
 {
+    private DisplayScore displayScoreScript;
+
+    private void Start()
+    {
+        displayScoreScript = GameObject.FindGameObjectWithTag("DisplayScoreText").GetComponent<DisplayScore>();
+    }
+
+
     private void OnTriggerEnter(Collider other) // other will be the animal that we hit with our food object
     {
+        displayScoreScript.score++;
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
