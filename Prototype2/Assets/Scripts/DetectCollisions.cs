8133cbf739479265773a1966d4f3b77892a9c208
TIGERJ1N <53501028+TIGERJ1N@users.noreply.github.com>
2024-09-14 13:19:51 -0500

    Prototype1 Progress3
    Populated the Prefabs folder created earlier with the food prefab and the animal prefabs.
    
    Added the following scripts: ShootPrefab to clone the food prefab and shoot them forward; MoveForward to allow the food prefab and the animals to move forward (food moves up, animals move down); DetectCollisions to allow the food prefab to detect when it has collided with an animal and destroy both itself and the animal; DestroyOutOfBounds to detect when food or animals are out of bounds, destroying them.

---
+++ Prototype2/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attach to food projectile prefab
+public class DetectCollisions : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) // other will be the animal that we hit with our food object
+    {
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Prototype2/Assets/Scripts/ShootPrefab.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attach this script to player
+public class ShootPrefab : MonoBehaviour
+{
+    public GameObject prefabToShoot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Create a clone of the prefab to shoot (food object) at the player's position using the prefab's rotation
+            Instantiate(prefabToShoot, transform.position, prefabToShoot.transform.rotation);
+        }
+    }
+}
